33aecbeafd196e2c033e27c3dedeef95e1a021d7
Amjadyabroudi128 <amjdayabroudi.2000@gmail.com>
2024-08-27 19:23:02 +0100

    fade in animation

--- lib/core/features/startingPage/startingPage.dart
+++ lib/core/features/startingPage/startingPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tic_tac_toe/core/features/startingPage/presentation/widgets/icons.dart';
+import 'package:tic_tac_toe/core/features/startingPage/presentation/widgets/startButton.dart';
 
 class startingPage extends StatefulWidget {
   const startingPage({super.key});
@@ -8,49 +9,46 @@ class startingPage extends StatefulWidget {
   @override
   State<startingPage> createState() => _startingPageState();
 }
+class _startingPageState extends State<startingPage> with SingleTickerProviderStateMixin{
+  late AnimationController controller;
+  late Animation<double> animation;
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+    animation = Tween<double>(begin: 0.0, end: 1.0).animate(controller);
 
-class _startingPageState extends State<startingPage> {
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red[400],
-      body:  SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 120, left: 70),
-              child: myIcons(),
-            ),
-            SizedBox(height: 460,),
-            startButton()
-          ],
+      body:  FadeTransition(
+        opacity: animation,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 120, left: 70),
+                child: myIcons(),
+              ),
+              SizedBox(height: 460,),
+              startButton()
+            ],
+          ),
         ),
       )
-    );
-  }
-}
-
-class startButton extends StatelessWidget {
-  const startButton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        minimumSize: Size(240, 68),
-        elevation: 8,
-        backgroundColor: Colors.yellow,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15) // Make the corners square
-        ),
-          side: BorderSide(color: Colors.black, width: 3,),
-      ),
-      onPressed: (){
-
-      },
-      child: Text("START", style: TextStyle(color: Colors.red[400], fontSize: 33, letterSpacing: 2.0),),
     );
   }
 }
