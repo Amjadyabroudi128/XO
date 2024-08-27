63f70d3681fb92935ca26e8d44364a3df23ab66b
Amjadyabroudi128 <amjdayabroudi.2000@gmail.com>
2024-08-27 18:32:06 +0100

    icon size and coloe

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,3 +1,5 @@
 
+import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-const FaIcon close = FaIcon(FontAwesomeIcons.xmark);
+const FaIcon close = FaIcon(FontAwesomeIcons.xmark, color: Colors.yellow, size: 170,);
+const FaIcon circle = FaIcon(FontAwesomeIcons.o, color: Colors.yellow,size: 150, );
