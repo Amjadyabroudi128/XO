9a385a333f3f7f5d672e455ace28fa6a0b7ff81b
Amjadyabroudi128 <amjdayabroudi.2000@gmail.com>
2024-08-27 19:22:48 +0100

    startButton

---
+++ lib/core/features/startingPage/presentation/widgets/startButton.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class startButton extends StatefulWidget {
+  const startButton({
+    super.key,
+  });
+
+  @override
+  State<startButton> createState() => _startButtonState();
+}
+
+class _startButtonState extends State<startButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(240, 68),
+        elevation: 8,
+        backgroundColor: Colors.yellow,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15) // Make the corners square
+        ),
+        side: BorderSide(color: Colors.black, width: 3,),
+      ),
+      onPressed: (){
+
+      },
+      child: Text("START", style: TextStyle(color: Colors.red[400], fontSize: 33, letterSpacing: 2.0),),
+    );
+  }
+}
