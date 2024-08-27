8aace825cc82f13cc4880c17a556aec394351444
Amjadyabroudi128 <amjdayabroudi.2000@gmail.com>
2024-08-27 19:00:22 +0100

    startButton

--- lib/core/features/startingPage/startingPage.dart
+++ lib/core/features/startingPage/startingPage.dart
@@ -14,16 +14,43 @@ class _startingPageState extends State<startingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red[400],
-      body:  const SingleChildScrollView(
+      body:  SingleChildScrollView(
         child: Column(
           children: [
             Padding(
-              padding: EdgeInsets.only(top: 150, left: 70),
+              padding: EdgeInsets.only(top: 120, left: 70),
               child: myIcons(),
-            )
+            ),
+            SizedBox(height: 460,),
+            startButton()
           ],
         ),
       )
+    );
+  }
+}
+
+class startButton extends StatelessWidget {
+  const startButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(240, 68),
+        elevation: 8,
+        backgroundColor: Colors.yellow,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15) // Make the corners square
+        ),
+          side: BorderSide(color: Colors.black, width: 3,),
+      ),
+      onPressed: (){
+
+      },
+      child: Text("START", style: TextStyle(color: Colors.red[400], fontSize: 33, letterSpacing: 2.0),),
     );
   }
 }
