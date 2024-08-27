b4e1fb0780fd6211f7441c9951dbe89dce601d3e
Amjadyabroudi128 <amjdayabroudi.2000@gmail.com>
2024-08-27 19:42:37 +0100

    constants colors

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// icons
 const FaIcon close = FaIcon(FontAwesomeIcons.xmark, color: Colors.yellow, size: 170,);
 const FaIcon circle = FaIcon(FontAwesomeIcons.o, color: Colors.yellow,size: 150, );
+// colors
+Color backGround = Colors.yellow;
--- lib/core/features/startingPage/presentation/widgets/startButton.dart
+++ lib/core/features/startingPage/presentation/widgets/startButton.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/constants/constants.dart';
 
 class startButton extends StatefulWidget {
   const startButton({
@@ -16,7 +17,7 @@ class _startButtonState extends State<startButton> {
       style: ElevatedButton.styleFrom(
         minimumSize: Size(240, 68),
         elevation: 8,
-        backgroundColor: Colors.yellow,
+        backgroundColor: backGround,
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15) // Make the corners square
         ),
