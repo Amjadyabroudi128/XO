67e92d699fe24326b2a9d1a7f9a6d15f5e2781ee
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-27 19:52:49 +0100

    colors

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,8 +1,5 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// icons
 const FaIcon close = FaIcon(FontAwesomeIcons.xmark, color: Colors.yellow, size: 170,);
 const FaIcon circle = FaIcon(FontAwesomeIcons.o, color: Colors.yellow,size: 150, );
-// colors
-Color backGround = Colors.yellow;
