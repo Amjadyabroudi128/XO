de0283ea2c11b193336d0e600c3edac3b7ad60d9
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-28 14:52:23 +0100

    google fonts

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 const FaIcon close = FaIcon(FontAwesomeIcons.xmark, color: Colors.yellow, size: 170,);
 const FaIcon circle = FaIcon(FontAwesomeIcons.o, color: Colors.yellow,size: 150, );
