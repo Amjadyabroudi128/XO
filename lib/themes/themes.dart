9214b9cf1fc827a5b1b7578efaa845eb272a3748
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-29 14:44:46 +0100

    made color global

---
+++ lib/themes/themes.dart
@@ -0,0 +1,11 @@
+
+
+ import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/constants/constants.dart';
+
+class AppTheme {
+
+  static final themeData = ThemeData().copyWith(
+    scaffoldBackgroundColor: redShade,
+  );
+ }
