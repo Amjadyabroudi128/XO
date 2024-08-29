d4aed4caaa1897f7c78dc9ff59853201096ab00e
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-29 18:07:06 +0100

    animations

--- lib/core/features/startingPage/startingPage.dart
+++ lib/core/features/startingPage/startingPage.dart
@@ -1,59 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tic_tac_toe/core/features/startingPage/presentation/widgets/icons.dart';
+import 'package:tic_tac_toe/core/features/startingPage/presentation/widgets/slidingIcons.dart';
+import 'package:tic_tac_toe/core/features/startingPage/presentation/widgets/slidingText.dart';
 import 'package:tic_tac_toe/core/features/startingPage/presentation/widgets/startButton.dart';
 
-class startingPage extends StatefulWidget {
+class startingPage extends StatefulWidget {  // Renamed to StartingPage
   const startingPage({super.key});
 
   @override
-  State<startingPage> createState() => _startingPageState();
+  State<startingPage> createState() => _StartingPageState();
 }
-class _startingPageState extends State<startingPage> with SingleTickerProviderStateMixin{
+
+class _StartingPageState extends State<startingPage> with TickerProviderStateMixin {
   late AnimationController controller;
+  late AnimationController iconsController;
+  late AnimationController textController;
   late Animation<double> animation;
+  late Animation<double> iconsAnimation;
+  late Animation<double> textAnimation;
+  late Animation<Offset> slideAnimation;
+  late Animation<Offset> iconsSlideAnimation;
+  late Animation<Offset> textSlideAnimation;
+
   @override
   void initState() {
+     final duration = AnimationController(
+       duration: const Duration(milliseconds: 600),
+       vsync: this,
+     );
+     final myController = Tween<double>(begin: 0.0, end: 1.0);
     super.initState();
-    controller = AnimationController(
-      duration: const Duration(milliseconds: 200),
-      vsync: this,
-    );
-    animation = Tween<double>(begin: 0.0, end: 1.0).animate(controller);
-
+    controller = duration;
+    animation = myController.animate(controller);
+    slideAnimation = Tween<Offset>(
+      begin: Offset(0.0, 1.0), // Start from below the screen (bottom)
+      end: Offset(0.0, 0.0),   // End at the original position
+    ).animate(controller);
     controller.forward();
+
+    iconsController = duration;
+    iconsAnimation = myController.animate(iconsController);
+    iconsSlideAnimation = Tween<Offset>(
+      begin: Offset(0.0, -1.0),
+      end: Offset(0.0, 0.0),
+    ).animate(iconsController);
+    iconsController.forward();
+
+    textController = duration;
+    textAnimation = myController.animate(textController);
+     textSlideAnimation = Tween<Offset>(begin: Offset(-1.0, 0.0),
+       end: Offset(0.0, 0.0),
+     ).animate(textController);
+     textController.forward();
   }
 
   @override
   void dispose() {
     controller.dispose();
+    iconsController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  FadeTransition(
-        opacity: animation,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 120, left: 70),
-                child: myIcons(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            slidingIcons(iconsSlideAnimation: iconsSlideAnimation, iconsAnimation: iconsAnimation),
+            tic_tac_toe(textSlideAnimation: textSlideAnimation, textAnimation: textAnimation),
+            const SizedBox(height: 380),
+            SlideTransition(
+              position: slideAnimation,
+              child: FadeTransition(
+                opacity: animation,
+                child: startButton(),  // Ensure this widget is correctly defined
               ),
-              Text("TIC-TAC-TOE", style: GoogleFonts.archivoBlack(
-                fontSize: 35,
-                color: Colors.white,
-                letterSpacing: 3
-              ),),
-              SizedBox(height: 380,),
-              startButton()
-            ],
-          ),
+            ),
+          ],
         ),
-      )
+      ),
     );
   }
 }
-
