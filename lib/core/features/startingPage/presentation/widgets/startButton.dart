41e9d2c8da58bd341f31f38e43ebde08ffe6982e
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-29 15:38:06 +0100

    global elevated button style

--- lib/core/features/startingPage/presentation/widgets/startButton.dart
+++ lib/core/features/startingPage/presentation/widgets/startButton.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:tic_tac_toe/constants/constants.dart';
 import 'package:tic_tac_toe/core/features/chooseSidePage/chooseSide.dart';
 
