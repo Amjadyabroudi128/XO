df98fd9c41625747ffb163e321357a7b9f21e2ce
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-29 18:06:28 +0100

    sliding icons

---
+++ lib/core/features/startingPage/presentation/widgets/slidingIcons.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'icons.dart';
+
+class slidingIcons extends StatelessWidget {
+  const slidingIcons({
+    super.key,
+    required this.iconsSlideAnimation,
+    required this.iconsAnimation,
+  });
+
+  final Animation<Offset> iconsSlideAnimation;
+  final Animation<double> iconsAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 120, left: 70),
+      child: SlideTransition(
+        position: iconsSlideAnimation,
+        child: FadeTransition(
+          opacity: iconsAnimation,
+          child: myIcons(),  // Ensure this widget is correctly defined
+        ),
+      ),
+    );
+  }
+}
