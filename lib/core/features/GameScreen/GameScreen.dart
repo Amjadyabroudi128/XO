1982205fd3a2eea1df168bf32dc658e65dfe404e
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-31 14:12:35 +0100

    game screen

--- lib/core/features/GameScreen/GameScreen.dart
+++ lib/core/features/GameScreen/GameScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../components/iconButton.dart';
 import '../../../constants/constants.dart';
 
 class GameScreen extends StatefulWidget {
@@ -25,25 +26,138 @@ class _GameScreenState extends State<GameScreen> {
   void initializeGame() {
     currentPlayer = widget.selectedSide;
     gameEnd = false;
-    occupied = ["", "", "", "", "", "", "", "", ""]; //9 empty places
+    occupied = ["", "", "", "", "", "", "", "", ""]; // 9 empty places
   }
+
+  void handleTap(int index) {
+    if (!gameEnd && occupied[index] == "") {
+      setState(() {
+        occupied[index] = currentPlayer;
+        if (checkWinner(currentPlayer)) {
+          gameEnd = true;
+          showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+              title: Text("Game Over"),
+              content: Text("Player $currentPlayer wins!"),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    initializeGame();
+                  },
+                  child: Text("Play Again"),
+                ),
+              ],
+            ),
+          );
+        } else if (!occupied.contains("")) {
+          gameEnd = true;
+          showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+              title: Text("Game Over"),
+              content: Text("It's a draw!"),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    initializeGame();
+                  },
+                  child: Text("Play Again"),
+                ),
+              ],
+            ),
+          );
+        } else {
+          currentPlayer = currentPlayer == "X" ? "O" : "X";
+        }
+      });
+    }
+  }
+
+  bool checkWinner(String player) {
+    const List<List<int>> winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (var combination in winningCombinations) {
+      if (occupied[combination[0]] == player &&
+          occupied[combination[1]] == player &&
+          occupied[combination[2]] == player) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Column(
         children: [
           Padding(
-            padding: EdgeInsets.only(left: 73, top: 63),
-            child: Text("TIC-TAC-TOE", style: slideText),
-          )
+            padding: EdgeInsets.only(left: 10, top: 63),
+            child: Row(
+              children: [
+                myButton(
+                  icon: goBack,
+                  onPressed: (){
+                    Navigator.of(context).pop();
+                  },
+                ),
+                SizedBox(width: 20,),
+                Text("TIC-TAC-TOE", style: slideText),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                ),
+                itemCount: 9,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () => handleTap(index),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.redAccent,
+                        borderRadius: BorderRadius.circular(8.0),
+
+                      ),
+                      child: Center(
+                        child: Text(
+                          occupied[index],
+                          style: TextStyle(
+                            fontSize: 70.0,
+                            fontWeight: FontWeight.bold,
+                            color: occupied[index] == "X"
+                                ? myYellow
+                                : myYellow,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
         ],
       ),
-      // body: Center(
-      //   child: Text(
-      //     'Player ${widget.selectedSide}\'s Turn',
-      //     style: TextStyle(fontSize: 30),
-      //   ),
-      // ),
     );
   }
 }
