67fc1a62bf9f8722e23adfe6bd609f50907103bd
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-02 14:28:22 +0100

    white color

--- lib/core/features/GameScreen/GameScreen.dart
+++ lib/core/features/GameScreen/GameScreen.dart
@@ -133,19 +133,19 @@ class _GameScreenState extends State<GameScreen> {
                     color: redShade, // Red background color
                     border: Border(
                       top: BorderSide(
-                        color: Colors.white,
+                        color: myWhite,
                         width: index > 2 ? 2 : 0, // Top line for rows 2 and 3
                       ),
                       left: BorderSide(
-                        color: Colors.white,
+                        color: myWhite,
                         width: index % 3 != 0 ? 2 : 0, // Left line for columns 2 and 3
                       ),
                       right: BorderSide(
-                        color: Colors.white,
+                        color: myWhite,
                         width: (index + 1) % 3 != 0 ? 2 : 0, // Right line for columns 1 and 2
                       ),
                       bottom: BorderSide(
-                        color: Colors.white,
+                        color: myWhite,
                         width: index < 6 ? 2 : 0, // Bottom line for rows 1 and 2
                       ),
                     ),
