c91d70b43a041156136c4e4fcaf647288263ab38
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-03 13:01:21 +0100

    win widget

--- lib/core/features/GameScreen/GameScreen.dart
+++ lib/core/features/GameScreen/GameScreen.dart
@@ -46,13 +46,7 @@ class _GameScreenState extends State<GameScreen> {
             context: context,
             builder: (_) {
               alertTimer(context);
-              return AlertDialog(
-              icon: Icon(Icons.thumb_up_outlined, size: 90,color: Colors.white,),
-              backgroundColor: Colors.transparent,
-              content: Text("Player $currentPlayer wins!", style: TextStyle(color: Colors.white, fontSize: 40),),
-              actions: [
-              ],
-            );
+              return winWidget(currentPlayer: currentPlayer);
               }
           );
         } else if (!occupied.contains("")) {
@@ -174,6 +168,26 @@ class _GameScreenState extends State<GameScreen> {
         ],
       ),
     );
+  }
+}
+
+class winWidget extends StatelessWidget {
+  const winWidget({
+    super.key,
+    required this.currentPlayer,
+  });
+
+  final String currentPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+    icon: Icon(Icons.thumb_up_outlined, size: 90,color: Colors.white,),
+    backgroundColor: Colors.transparent,
+    content: Text("Player $currentPlayer wins!", style: TextStyle(color: Colors.white, fontSize: 40),),
+    actions: [
+    ],
+                );
   }
 }
 
