d562f5c26a099a94128ab25f3a2a22aa9a9078f4
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-10 14:45:04 +0100

    start container

--- lib/core/features/startingPage/presentation/widgets/startButton.dart
+++ lib/core/features/startingPage/presentation/widgets/startButton.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tic_tac_toe/components/elevatedButton.dart';
 import 'package:tic_tac_toe/constants/constants.dart';
 import 'package:tic_tac_toe/core/features/chooseSidePage/chooseSide.dart';
+import 'package:tic_tac_toe/core/features/startingPage/presentation/widgets/startContainer.dart';
 
 class startButton extends StatefulWidget {
   const startButton({
@@ -18,30 +19,5 @@ class _startButtonState extends State<startButton> {
   Widget build(BuildContext context) {
     String start = "START";
     return startContainer(start: start);
-  }
-}
-
-class startContainer extends StatelessWidget {
-  const startContainer({
-    super.key,
-    required this.start,
-  });
-
-  final String start;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        border: startBorder
-      ),
-      child: kElevatedButton(
-        onPressed: (){
-          Navigator.of(context).pushNamed(chooseSide.id);
-        },
-        child: Text(start, style: googleStyle,),
-      ),
-    );
   }
 }
