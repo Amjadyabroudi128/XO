9c584de84401f37fab43871abbdb2aae49cae420
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-12 15:49:39 +0100

    alert component

--- lib/components/AlertDialog.dart
+++ lib/components/AlertDialog.dart
@@ -4,13 +4,14 @@ class Alert extends StatelessWidget {
   final Widget? icon;
   final Text? text;
   final List<Widget>? actions;
-  const Alert({super.key, this.icon, this.text, this.actions});
+  final Widget? myContext;
+  const Alert({super.key, this.icon, this.text, this.actions, this.myContext});
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       icon: icon,
-      content: text,
+      content: myContext,
       actions: actions,
     );
   }
