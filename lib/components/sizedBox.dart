2938e8a338dee6be43b2d4b90c42be2efef1cd74
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-13 13:16:23 +0100

    sizeBox

---
+++ lib/components/sizedBox.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class mySpace extends StatelessWidget {
+  const mySpace({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
