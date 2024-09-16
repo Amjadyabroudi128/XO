b0615fd2391be136a28487b7b76176616a76680a
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-16 12:33:21 +0100

    backbtn

---
+++ lib/core/features/chooseSidePage/backBtn.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../../../components/iconButton.dart';
+import '../../../constants/constants.dart';
+
+backBtn(BuildContext context) {
+  return myButton(
+    icon: goBack,
+    onPressed: (){
+      Navigator.of(context).pop();
+    },
+  );
+}
