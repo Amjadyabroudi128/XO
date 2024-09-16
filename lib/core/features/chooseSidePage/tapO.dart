842de330f76ee7dc1eb6cef18a1da0b87f897d21
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-16 12:31:22 +0100

    tapO

---
+++ lib/core/features/chooseSidePage/tapO.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../components/chooseSide.dart';
+import '../../../components/padding.dart';
+import 'buildChoiceButton.dart';
+
+tapO(BuildContext context) {
+  return GestureDetector(
+    onTap: () {
+      pickSide(context, "O");
+    },
+    child: MyPadding(
+      pad: const EdgeInsets.only(top: 20),
+      child: buildChoiceButton(FontAwesomeIcons.o),
+    ),
+  );
+}
