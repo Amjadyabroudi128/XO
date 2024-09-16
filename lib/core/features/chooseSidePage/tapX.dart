38f1c858f661babc68dc36b2b2c936accbcb2b89
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-16 12:29:43 +0100

    tapX

---
+++ lib/core/features/chooseSidePage/tapX.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../components/chooseSide.dart';
+import '../../../components/padding.dart';
+import 'buildChoiceButton.dart';
+
+tapX(BuildContext context) {
+  return GestureDetector(
+    onTap: (){
+      pickSide(context, "X");
+    },
+    child: MyPadding(
+      pad:  EdgeInsets.only(top: 80),
+      child: buildChoiceButton(FontAwesomeIcons.xmark),
+    ),
+  );
+}
