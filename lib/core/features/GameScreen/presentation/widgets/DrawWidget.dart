b51991ba5913cbb1c3d4158ed2a63a175a71b6a7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-17 13:03:38 +0100

    my padding

--- lib/core/features/GameScreen/presentation/widgets/DrawWidget.dart
+++ lib/core/features/GameScreen/presentation/widgets/DrawWidget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tic_tac_toe/components/AlertDialog.dart';
+import 'package:tic_tac_toe/components/padding.dart';
 import 'package:tic_tac_toe/constants/constants.dart';
 
 class DrawWidget extends StatelessWidget {
@@ -11,12 +12,12 @@ class DrawWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     String draw = "It's a Draw!";
     return  Alert(
-      icon: const Padding(
-        padding: EdgeInsets.only(left: 70),
+      icon: const MyPadding(
+        pad: EdgeInsets.only(left: 70),
         child: frown,
       ),
-      myContext:Padding(
-      padding: const EdgeInsets.only(left: 20),
+      myContext:MyPadding(
+      pad: const EdgeInsets.only(left: 20),
       child: Text(draw, style: alertDialog),
     ),
     );
