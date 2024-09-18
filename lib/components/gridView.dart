b9f8ce7c09695222dec6810fe9001afaa5d134d6
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-18 12:53:04 +0100

    gridView

--- lib/components/gridView.dart
+++ lib/components/gridView.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 class gameBox extends StatelessWidget {
-  const gameBox({super.key});
+  final Widget Function(BuildContext, int) building;
+  const gameBox({super.key, required this.building});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3, // 3 columns
+        mainAxisSpacing: 2.0,
+        crossAxisSpacing: 2.0,
+      ),
+      itemCount: 9,
+      itemBuilder: building,
+    );
   }
 }
