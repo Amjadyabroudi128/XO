93b03c9bfccbed188e2da16b026f2f407fa56746
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-21 12:18:48 +0100

    zap Navigation

--- lib/components/chooseSide.dart
+++ lib/components/chooseSide.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:zapx/zapx.dart';
 import '../core/features/GameScreen/GameScreen.dart';
 
 void pickSide(BuildContext context, String selectedSide) {
-  Navigator.push(
-    context,
-    MaterialPageRoute(
-      builder: (context) => GameScreen(selectedSide: selectedSide),
-    ),
-  );
+  // Navigator.push(
+  //   context,
+  //   MaterialPageRoute(
+  //     builder: (context) => GameScreen(selectedSide: selectedSide),
+  //   ),
+  // );
+  Zap.to(GameScreen(selectedSide: selectedSide));
 }
